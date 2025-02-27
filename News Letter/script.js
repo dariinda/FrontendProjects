b8724567aa699ab731a592562cd902b27493d714
Abhishek Singh <116362411+dariinda@users.noreply.github.com>
2025-02-27 18:31:31 +0530

    Add files via upload

--- News Letter/script.js
+++ News Letter/script.js
@@ -4,6 +4,10 @@ function validateEmail() {
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   if (validRegex.test(input)) {
     console.log("success");
+    document.getElementsByTagName("main")[0].style.display = "none";
+    document.getElementById("thanksCard").style.display = "block";
+    document.getElementById("mailShown").innerText = input;
+
   } else {
     document.getElementById("errorText").hidden = false;
     let box = document.getElementById("mailInput");
@@ -19,3 +23,12 @@ function hideErrorText() {
   box.style.backgroundColor = "white";
   box.style.border = "1px solid gray";
 }
+
+
+function toMailShown(){
+  document.getElementById("thanksCard").style.display = "none";
+  document.querySelector("main").style.display = "flex";
+  document.getElementById("mailInput").value = "";
+}
+
+
