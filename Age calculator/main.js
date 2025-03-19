0ad9c2a9a394b4c75e25f728c3e75bdeebad5506
Abhishek Singh <116362411+dariinda@users.noreply.github.com>
2025-03-19 23:16:19 +0530

    Add files via upload

--- Age calculator/main.js
+++ Age calculator/main.js
@@ -6,8 +6,6 @@ let ageMonthsElement = document.getElementById("ageMonths");
 let ageDaysElement = document.getElementById("ageDays");
 const button = document.getElementById("button");
 
-console.log(`${day}`);
-
 button.addEventListener("click", () => {
   let day = parseInt(dayElement.value);
   let month = parseInt(monthElement.value);
@@ -18,13 +16,41 @@ button.addEventListener("click", () => {
   const currMonth = today.getMonth() + 1;
   const currDay = today.getDate();
 
+  let dayErrorMessage = document.getElementById("dayErrorMessage");
+  let monthErrorMessage = document.getElementById("monthErrorMessage");
+  let yearErrorMessage = document.getElementById("yearErrorMessage");
+
+  dayErrorMessage.textContent = "";
+  monthErrorMessage.textContent = "";
+  yearErrorMessage.textContent = "";
+  dayElement.style.border="2px "
+
+  if (isNaN(day) || day <= 0) {
+    dayErrorMessage.textContent = "Enter a valid day";
+    dayElement.style.border="2px solid red";
+    document.querySelector('label[for="day"]').style.color="red";
+  }
+
+  if (isNaN(month) || month <= 0 || month > 12) {
+    monthErrorMessage.textContent = "Enter a valid month";
+    monthElement.style.border="2px solid red";
+    document.querySelector('label[for="month"]').style.color="red";
+
+  }
+
+  if (isNaN(year) || year <= 0 || year > currYear) {
+    yearErrorMessage.textContent = "Enter a valid year";
+    yearElement.style.border="2px solid red";
+    document.querySelector('label[for="year"]').style.color="red";
+  }
+
   let ageYear = currYear - year;
   let ageMonth = currMonth - month;
   let ageDay = currDay - day;
 
   if (ageDay < 0) {
     ageMonth -= 1;
-    ageDay += new Date(currYear, currMonth - 1, 0).getDate(); // Get days in the previous month
+    ageDay += new Date(currYear, currMonth - 1, 0).getDate(); 
   }
   if (ageMonth < 0) {
     ageYear -= 1;
